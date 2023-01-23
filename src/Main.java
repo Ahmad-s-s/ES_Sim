32acb979aa53075c0551e07e88af84328deb6fc8
Ahmad-s-s <ahmadmshams2003@gmail.com>
2023-01-23 18:24:48 +0330

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
 
 
+import Var.Icons;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        System.out.println("Hello world!");
+//        JOptionPane.showMessageDialog(null, "Hello", "title", JOptionPane.PLAIN_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello", "title", JOptionPane.INFORMATION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello", "title", JOptionPane.QUESTION_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello", "title", JOptionPane.WARNING_MESSAGE);
+//        JOptionPane.showMessageDialog(null, "Hello", "title", JOptionPane.ERROR_MESSAGE);
+
+//        JOptionPane.showConfirmDialog(null, "bro did you code ?",
+//                "title", JOptionPane.OK_CANCEL_OPTION);
+//        JOptionPane.showConfirmDialog(null, "Q?", "title", JOptionPane.YES_NO_CANCEL_OPTION);
+
+//        System.out.println(JOptionPane.showInputDialog(null, "Your name : ", "title",
+//                JOptionPane.INFORMATION_MESSAGE));
+
+        String[] responses = {"option 1", "option 2", "option 3"};
+        JOptionPane.showOptionDialog(null,
+                "Add test", "Title", JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.INFORMATION_MESSAGE, Icons.add, responses, responses[0]);
     }
+
 }
---
+++ src/Var/Icons.java
@@ -0,0 +1,14 @@
+package Var;
+
+import javax.swing.*;
+
+public class Icons {
+    public static ImageIcon add = new ImageIcon("Var/icons8-add-64.png");
+    public static ImageIcon done = new ImageIcon("icons8-done-64.png");
+    public static ImageIcon save = new ImageIcon("Var/icons8-save-64.png");
+    public static ImageIcon error = new ImageIcon("Var/icons8-error-64.png");
+    public static ImageIcon privacy = new ImageIcon("Var/icons8-privacy-64.png");
+    public static ImageIcon list = new ImageIcon("Var/icons8-list-64.png");
+    public static ImageIcon delete = new ImageIcon("Var/delete-forever.png");
+
+}
